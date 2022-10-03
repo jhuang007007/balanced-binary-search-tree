97e31028dbe86fa6dccad694154207d75a636c61
johnny <jhuang007007@gmail.com>
2022-10-03 08:49:52 -0400

    insert function takes a value and inserts it into the tree such that it is still balanced

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // /Build a Node class / factory. It should have an attribute for the data it stores as well as its left and right children.
-function nodeFactory(data, left, right) {
+function nodeFactory(data, left = null, right = null) {
   return {
     data,
     left,
@@ -14,9 +14,29 @@ function treeFactory(array) {
   let end = formattedArray.length - 1;
   let root = buildTree(formattedArray, 0, end);
 
+  const insert = (data) => {
+    root = _insertRecursive(root, data)
+    prettyPrint(root)
+  }
+
+  const _insertRecursive = (root, data) => {
+    //base case 
+    if (root === null) {
+      root = nodeFactory(data)
+      return root;
+    }
+
+    //recur down the tree if node is not empty
+    if (data < root.data) root.left = _insertRecursive(root.left, data);
+    else if (data > root.data) root.right = _insertRecursive(root.right, data);
+      
+    return root;
+  }
+
   prettyPrint(root)
   return {
-    root
+    root,
+    insert
   }
 }
 
@@ -56,5 +76,25 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
   }
 }
 
+function insert(data) {
+  root = insertRecursive(root, data)
+}
+
+function insertRecursive(root, data) {
+  //base case 
+  if (root === null) {
+    root = nodeFactory(data)
+    return root;
+  }
+
+  //recur down the tree if node is not empty
+  if (root < root.data) root.left = insertRecursive(root.left, data);
+  else if (key > root.data) root.right = insertRecursive(root.right, key);
+    
+  return root;
+}
+
 let testTree1 = treeFactory([1,2,3,4,5,6,7,8,9])
 let testTree2 = treeFactory([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
+testTree1.insert(10)
